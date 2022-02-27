768341ff0861e5ef905c062013a4f427dbdbc6e8
pepe lopez <plopez741@gmail.com>
2022-02-26 16:39:43 -0800

    commit tree 1c222a420b36ceeb25a29f11591c0f0ad868613e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function superbowlWin(obj){
+   
+    for(const item of obj){
+      let objWin= obj.find(() => item.result === "W")   
+      if (objWin !== undefined){
+         return (item.year) 
+      }
+      
+    }
+    
+    
+}
+
+const record = [
+    { year: "2014", result: "N/A"},
+   
+    { year: "2013", result: "L"},
+     { year: "2015", result: "W"},
+    //...
+  
+]
+superbowlWin(record)
